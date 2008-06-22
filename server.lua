2e4a8a75e92ef8095f0fc8edd54485657732d3eb
daurnimator <daurnimator@8385cabc-f040-0410-9bc4-65f6fe3c7374>
2008-06-22 04:31:23 +0000

    + Added License to all files (except html client)  * Updated README for new/changed dependencies
    git-svn-id: http://lomp.googlecode.com/svn/trunk@37 8385cabc-f040-0410-9bc4-65f6fe3c7374

--- server.lua
+++ server.lua
@@ -1,5 +1,13 @@
---local format, gsub, strfind, strsub = string.format, string.gsub, string.find, string.sub
---local concat, getn, tinsert = table.concat, table.getn, table.insert
+--[[
+	LOMP ( Lua Open Music Player )
+	Copyright (C) 2007- daurnimator
+
+	This program is free software: you can redistribute it and/or modify it under the terms of version 3 of the GNU General Public License as published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,	but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
 
 pcall ( require , "luarocks.require" )
 
