4cb6ffc3c2d1cb45d447f8106aead676e70e0106
daurnimator <daurnimator@8385cabc-f040-0410-9bc4-65f6fe3c7374>
2008-11-30 14:49:48 +0000

    + Got flac working
    git-svn-id: http://lomp.googlecode.com/svn/trunk@48 8385cabc-f040-0410-9bc4-65f6fe3c7374

--- modules/fileinfo/flac.lua
+++ modules/fileinfo/flac.lua
@@ -9,33 +9,29 @@ print(file)
 fd = io.open ( file , r )
 local s = fd:read ( 4 )
 
---fileinfo = { }
-
 -- Format info found at http://flac.sourceforge.net/format.html
 if s == "fLaC" then 
 	filetyp = "flac"
 	local t
-
+	local sampleinfo = { }
+	local tags = { }
 	repeat
-		t = vstruct.unpack ( "> m1 u3" , fd )
+		t = vstruct.unpack ( "< m1 > u3" , fd )
 		
-		local lastmetadatablock = vstruct.implode { unpack ( t [ 1 ] , 1 , 1 ) }
-		local blocktype = vstruct.implode { unpack ( t [ 1 ] , 2 , 8 ) }
+		local lastmetadatablock = vstruct.implode { unpack ( t [ 1 ] , 8 , 8 ) }
+		local blocktype = vstruct.implode { unpack ( t [ 1 ] , 1 , 7 ) }		
 		local blocklength = t [ 2 ] -- Is in bytes
 		
-		print ( lastmetadatablock , blocktype , blocklength )
-		
 		if blocktype == 0 then -- Stream info
-			t = vstruct.unpack ( "> u2 u2 u3 u3 m8 u16" , fd )
-			sampleinfo = { }
+			t = vstruct.unpack ( "> u2 u2 u3 u3 <m8> u16" , fd )
 			sampleinfo.minblocksize = t [ 1 ]
 			sampleinfo.maxblocksize = t [ 2 ]
 			sampleinfo.minframesize = t [ 3 ]
 			sampleinfo.maxframesize = t [ 4 ]
-			sampleinfo.samplerate = vstruct.implode { unpack ( t [ 5 ] , 1 , 20 ) }
-			sampleinfo.channels = vstruct.implode { unpack ( t [ 5 ] , 21 , 23 ) } + 1
-			sampleinfo.bitspersample = vstruct.implode { unpack ( t [ 5 ] , 24 , 28 ) }
-			sampleinfo.totalsamples = vstruct.implode { unpack ( t [ 5 ] , 29 , 64 ) }
+			sampleinfo.samplerate = vstruct.implode { unpack ( t [ 5 ] , 45 , 64 ) }
+			sampleinfo.channels = vstruct.implode { unpack ( t [ 5 ] , 42 , 44 ) } + 1
+			sampleinfo.bitspersample = vstruct.implode { unpack ( t [ 5 ] , 37 , 41 ) }
+			sampleinfo.totalsamples = vstruct.implode { unpack ( t [ 5 ] , 1 , 36 ) }
 			sampleinfo.md5rawaudio = t [ 6 ]
 			--t [ 1 ] = nil
 			--samplerate = t [ 5 ]
@@ -44,26 +40,25 @@ if s == "fLaC" then
 			--totalsamples = t [ 8 ]
 			for k,v in pairs ( sampleinfo ) do print ( k,v) end
 		elseif blocktype == 1 then -- Padding
-			t = vstruct.unpack ( ">x" .. blocklength , fd )
+			t = vstruct.unpack ( "> x" .. blocklength , fd )
 		elseif blocktype == 2 then -- Application
-			t = vstruct.unpack ( ">u4s" .. ( blocklength - 4 ) , fd )
+			t = vstruct.unpack ( "> u4 s" .. ( blocklength - 4 ) , fd )
 			appID = t [ 1 ]
 			appdata = t [ 2 ]
 		elseif blocktype == 3 then -- Seektable
-			t = vstruct.unpack ( ">x" .. blocklength , fd ) -- We don't deal with seektables, skip over it
+			t = vstruct.unpack ( "> x" .. blocklength , fd ) -- We don't deal with seektables, skip over it
 		elseif blocktype == 4 then -- Vorbis_Comment http://www.xiph.org/vorbis/doc/v-comment.html
-			t = vstruct.unpack ( "<u4" , fd )
+			t = vstruct.unpack ( "< u4" , fd )
 			vendor_length = t [ 1 ]
 			t = vstruct.unpack ( "<s" .. vendor_length .. "u4" , fd )
 			vendor_string = t [ 1 ]
 			user_comment_list_length = t [ 2 ]
 			
 			comment = { }
-			tags = { }
-			for i = 1 , ( user_comment_list_length + 1 ) do
-				t = vstruct.unpack ( "<u4" , fd )
+			for i = 1 , ( user_comment_list_length ) do
+				t = vstruct.unpack ( "< u4" , fd )
 				local length = t [ 1 ]
-				t = vstruct.unpack ( "<s" .. length , fd )
+				t = vstruct.unpack ( "< s" .. length , fd )
 				comment [ i ] = t [ 1 ]
 				local fieldname , value = string.match ( comment [ i ] , "([^=]+)=(.+)")
 				tags [ fieldname ] = tags [ fieldname ] or { }
@@ -88,7 +83,13 @@ if s == "fLaC" then
 			t = vstruct.unpack ( ">s" .. picturelength , fd )
 			picturedata = t [ 1 ]
 		end
-	
 	until lastmetadatablock == 1
+	local item = { }
+	item.format = "flac"
+	item.length = sampleinfo.totalsamples / sampleinfo.samplerate
+	item.tags = tags
+	item.extra = sampleinfo
+	
+	return item
 end
 fd:close ( )
