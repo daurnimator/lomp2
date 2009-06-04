051e633cb91b04683b152b46b39b6e435ae6bb8c
daurnimator <daurnimator@8385cabc-f040-0410-9bc4-65f6fe3c7374>
2009-06-04 14:17:56 +0000

    * More work on javascript
    git-svn-id: http://lomp.googlecode.com/svn/trunk@102 8385cabc-f040-0410-9bc4-65f6fe3c7374

--- clients/javascript/lomp.js
+++ clients/javascript/lomp.js
@@ -3,9 +3,57 @@ function LOMP ( server ) {
 	
 	this.server = server;
 	this.jsonurl = "/JSON";
+	
+	var statecallbacks = { children : { } , callbacks : [ ] };
+	this.addcallback = function ( onfield , func ) {
+		var field = statecallbacks;
+		for ( var i = 0  ; i < ( onfield.length - 1 ) ; i++ ) {
+			var notexist
+			if ( typeof field [ onfield [ i + 1 ] ] == "number" ) { notexist = { children : [ ] , callbacks : [ ] }; }
+			else { notexist = { children : { } , callbacks : [ ] }; };
+			
+			field.children [ onfield [ i ] ] = field.children [ onfield [ i ] ] || notexist;
+			field = field.children [ onfield [ i ] ];
+		};
+		field.callbacks.push ( func );
+	};
+	var docallbacks = function ( callbacktable , onfield , data ) {
+		for ( var i = 0 ; i < callbacktable.length ; i++ ) {
+			callbacktable [ i ] ( data );
+		};
+	}
+	
+	var processcallbacks = function ( onfield , data ) {
+		var field = statecallbacks;
+		docallbacks ( field.callbacks , onfield , data );
+		for ( var i = 0  ; i < ( onfield.length - 1 ) ; i++ ) {
+			if ( ! field.children [ onfield [ i ] ] ) { break; };
+			field = field.children [ onfield [ i ] ];
+			docallbacks ( field.callbacks , onfield , data );
+		};
+		//docallbacks ( field.callbacks , onfield , data );
+	}
+	
 	this.state = { };
 	this.state.playlistinfo = [ ];
 	this.state.playlists = [ ];
+	
+	this.editstate = function ( onfield , data ) {
+		var field = this.state;
+		for ( var i = 0  ; i < ( onfield.length - 1 ) ; i++ ) {
+			var notexist
+			if ( typeof field [ onfield [ i + 1 ] ] == "number" ) { notexist = [ ]; }
+			else { notexist = { }; };
+			
+			field [ onfield [ i ] ] = field [ onfield [ i ] ] || notexist;
+			field = field [ onfield [ i ] ];
+		};
+		var subfield = onfield [ onfield.length - 1 ];
+		field [ subfield ] = data;
+		//console.log(subfield , data)
+		processcallbacks ( onfield , data ); 
+	};
+	
 	this.sendqueue = { };
 
 	var send = function ( object , callback ) {
@@ -18,7 +66,7 @@ function LOMP ( server ) {
 				that.sendqueue [ k ] = object [ k ];
 			};
 		};
-	}
+	};
 	var processqueue = function ( ) {
 		var sendqueue = that.sendqueue;
 		that.sendqueue = { };
@@ -28,13 +76,13 @@ function LOMP ( server ) {
 			if ( !tobesent [ k ] ) tobesent [ k ] = [ ];
 			for ( var i in sendqueue [ k ] ) {
 				var a = { };
-				a [ k ] = sendqueue [ k ] [ i ] [ k ]
-				a.params = sendqueue [ k ] [ i ] .params
-				tobesent [ k ].push ( a )
-			}
-		}
+				a [ k ] = sendqueue [ k ] [ i ] [ k ];
+				a.params = sendqueue [ k ] [ i ] .params;
+				tobesent [ k ].push ( a );
+			};
+		};
 		
-		tobesent = JSON.stringify ( tobesent )
+		tobesent = JSON.stringify ( tobesent );
 		
 		$.post (  that.jsonurl , tobesent , function ( data ) {
 			for ( var i = 0 ; i < data.length ; i++ ) {
@@ -43,32 +91,33 @@ function LOMP ( server ) {
 				} else {
 					console.log ( "FAILED" , data [ i ] [ 1 ] )
 					sendqueue.cmd [ i ].failcallback ( data [ i ] [ 1 ] );
-				}
-			}
+				};
+			};
+			//that.refresh();
 		} , "json" );
-	}
+	};
 	
 	this.getPlaylistInfo = function ( ) {
 		send ( { cmd : [ 
 			{ cmd : "core.info.getplaylistinfo" , params : [ 0 ] , successcallback : function ( data ) {
-				that.state.libraryinfo = data [ 0 ];
+				that.editstate ( [ "libraryinfo" ] , data [ 0 ] );
 			} }, //Library
 			{ cmd : "core.info.getlistofplaylists" , params : [ ] , successcallback : function ( data ) {
-				that.state.playlistinfo = data [ 0 ];
+				that.editstate ( [ "playlistinfo" ] , data [ 0 ] );
 			} }, //All the other playlists
 		] } );
 	};
 	this.updatePlaylist = function ( playlist ) {
 		send ( { cmd : [ 
 			{ cmd : "core.info.getplaylist" , params : [ playlist + 1 ] , successcallback : function ( data ) {
-				that.state.playlists [ playlist ] = data [ 0 ];
+				that.editstate ( [ "playlists" , playlist ] , data [ 0 ] );
 			} }
 		] } );
 	};
 	this.updateLibrary = function ( ) {
 		send ( { cmd : [ 
 			{ cmd : "core.info.getplaylist" , params : [ 0 ] , successcallback : function ( data ) {
-				that.state.library = data [ 0 ];
+				that.editstate ( [ "library" ] , data [ 0 ] );
 			} }
 		] } );
 	};
@@ -95,5 +144,5 @@ function LOMP ( server ) {
 	};
 	
 	// Setup refreshing
-	this.timer = setInterval ( function ( ) { that.refresh ( ); } , 2000 );
+	//this.timer = setInterval ( function ( ) { that.refresh ( ); } , 2000 );
 }
