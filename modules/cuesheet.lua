9279371f091be2de8ff0b6d2df0b04281c14a580
daurnimator <daurnimator@8385cabc-f040-0410-9bc4-65f6fe3c7374>
2010-01-07 17:07:40 +0000

    + Updated readme to require lpeg + cuesheet: Noncompliant ones (like from EAC) are now accepted, bugs fixed
    git-svn-id: http://lomp.googlecode.com/svn/trunk@214 8385cabc-f040-0410-9bc4-65f6fe3c7374

--- modules/cuesheet.lua
+++ modules/cuesheet.lua
@@ -21,21 +21,20 @@ module ( "lomp.cuesheet" , package.see ( lomp ) )
 -- All audio files (WAVE, AIFF, and MP3) must be in 44.1KHz 16-bit stereo format
 -- For AUDIO files, if the length of the data within the file is not an exact multiple of the CDROM sector size (2352 bytes), then the last sector will be padded with zeros when it is recorded to the blank disc.
 
-local lineparser
-local commentparser
+--local lineparser , commentparser
 do
 	local lpeg = require "lpeg"
 	local C , Cc , P , R , S = lpeg.C , lpeg.Cc , lpeg.P , lpeg.R , lpeg.S
 	
-	local eos = -P ( 1 )
-	local space = S ( ' ' )^1
+	local eos = P ( '\r' )^-1 * -P ( 1 )
+	local space = S ( ' ' )
 	local digit = R ( '09' )
 	local lower , upper = R ( 'az' ) , R ( 'AZ' )
 	local alpha = lower + upper
 	local alphanumeric = digit + alpha
 	
 	local str = '"' * C ( ( P ( 1 ) - '"')^0 ) * '"' -- Quoted string
-		+ C ( ( 1 - space + ' \n' )^0 ) -- normal string
+		+ C ( ( 1 - space - eos )^0 ) -- normal string
 		
 	local timestamp = ( C ( ( digit * digit ) / tonumber ) * ":" * C ( ( digit * digit ) / tonumber ) * ":" * C ( ( digit * digit ) / tonumber ) ) / function ( mins , secs , frames ) return frames + 75 * ( secs + 60 * mins ) end + function ( _ , i ) error ( "Invalid timestamp in cuesheet" ) end -- Returns number of frames
 	
@@ -51,11 +50,12 @@ do
 	local flag = C ( P "DCP" + "4CH" + "PRE" + "SCMS" ) + function ( _ , i ) error ( "Invalid flag in cuesheet" ) end
 	local FLAGS = C "FLAGS" * ( space * flag )^1
 	
-	local indexnumber = C ( digit * digit^-1 / tonumber )
+	local indexnumber = digit * digit^-1 / tonumber
 	local INDEX = C "INDEX" * space * indexnumber * space * timestamp
 	
-	local isrccode = C ( alphanumeric * alphanumeric * alphanumeric * alphanumeric * alphanumeric * digit * digit * digit * digit * digit * digit * digit / strupper )
-	local ISRC = C "ISRC" * space * isrccode
+	local isrccode = alphanumeric * alphanumeric * alphanumeric * alphanumeric * alphanumeric * digit * digit * digit * digit * digit * digit * digit / strupper
+	local isrcwtf = ( P "GAPc;" + "FQ`^7" ) * digit * digit * digit * digit * digit * digit * digit * Cc ( nil ) -- Weird ISRC patterns that seems to show up...
+	local ISRC = C "ISRC" * space * ( isrccode + isrcwtf )
 	
 	local PERFORMER = C "PERFORMER" * space * str
 	
@@ -69,12 +69,14 @@ do
 	
 	local TITLE = C "TITLE" * space * str
 	
-	local datatype = C ( P "AUDIO" + "CDG" + "MODE1/2048" + "MODE1/2352" + "MODE2/2336" + "MODE2/2352" + "CDI/2336" + "CDI/2352" ) + function ( _ , i ) error ( "Invalid track datatype in cuesheet" ) end
+	local datatype = C ( P "AUDIO" + "CDG" + "MODE1/2048" + "MODE1/2352" + "MODE2/2336" + "MODE2/2352" + "CDI/2336" + "CDI/2352" + "MODEx/2xxx" ) + function ( _ , i ) error ( "Invalid track datatype in cuesheet" ) end -- Note: "MODEx/2xxx" isn't in spec, it sometimes comes up for CD-Extra
 	local TRACK = C "TRACK" * space * indexnumber * space * datatype
 	
-	lineparser = space^0 * ( CATALOG + CDTEXTFILE + FILE + FLAGS + INDEX + ISRC + PERFORMER + POSTGAP + PREGAP + REM + SONGWRITER + TITLE + TRACK )^-1 * space^0 * eos + function ( _ , i ) error ( "Invalid cuesheet" ) end
+	lineparser = P("\239\187\191" )^0 -- Some files get the utf bom... wtf!
+		* space^0 * ( CATALOG + CDTEXTFILE + FILE + FLAGS + INDEX + ISRC + PERFORMER + POSTGAP + PREGAP + REM + SONGWRITER + TITLE + TRACK )^-1 
+		* space^0 * eos + function ( _ , i ) error ( "Invalid cuesheet" ) end
 	
-	commentparser = C ( upper^1 ) * space^1 * str
+	commentparser = C ( upper^1 ) * space * str
 end
 
 local d = {
@@ -88,14 +90,14 @@ local d = {
 	CDTEXTFILE = function ( data , path ) end ; -- NO-OP
 	FILE = function ( data , path , typ )
 		local files = data.files
-		files [ #files + 1 ] = { path = path , type = typ , tracks = { } }
+		files [ #files + 1 ] = { path = path , type = typ , tracks = { n = 0 } }
 	end ;
 	FLAGS = function ( data , ... )
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				local flags = tracks [ track ].flags
+			if tracks.n > 0 then
+				local flags = tracks [ tracks.n ].flags
 				for i , v in ipairs ( { ... } ) do
 					flags [ v ] = true
 				end
@@ -108,14 +110,32 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				local indexes = tracks [ track ].indexes
+			if tracks.n > 0 then
+				local indexes = tracks [ tracks.n ].indexes
 				if ( #indexes == 0 and ( index == 0 or index == 1 ) ) or #indexes == ( index - 1 ) then
 					indexes [ index ] = frames
 				else
 					error ( "Invalid INDEX (" .. index .. ") in cuesheet" )
 				end
 				return
+			else
+				local lastfile = files [ #files - 1 ]
+				if lastfile then
+					local lasttracks = lastfile.tracks
+					local lasttrack = lasttracks [ lasttracks.n ]
+					if lasttrack then
+						local indexes = lasttrack.indexes
+						if indexes [ index - 1 ] then
+							-- Non-compliant: but we allow a single track distributed over multiple files
+							data.noncompliant = true
+							
+							tracks.n = 1
+							tracks [ 1 ] = lasttrack
+							indexes [ index ] = frames
+							return
+						end
+					end
+				end
 			end
 		end
 		error ( "INDEX can only appear after TRACK in a cuesheet" )
@@ -124,8 +144,8 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				tracks [ track ].isrc = isrc
+			if tracks.n > 0 then
+				tracks [ tracks.n ].isrc = isrc
 				return
 			end
 		end
@@ -135,8 +155,8 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				tracks [ track ].performer = performer
+			if tracks.n > 0 then
+				tracks [ tracks.n ].performer = performer
 				return
 			end
 		end
@@ -146,8 +166,8 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				local track = tracks [ #tracks ]
+			if tracks.n > 0 then
+				local track = tracks [ tracks.n ]
 				if track.postgap then
 					error ( "POSTGAP can only appear once for each TRACK" )
 				else
@@ -162,8 +182,8 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				local track = tracks [ #tracks ]
+			if tracks.n > 0 then
+				local track = tracks [ tracks.n ]
 				if track.pregap then
 					error ( "PREGAP can only appear once for each TRACK" )
 				else
@@ -188,8 +208,8 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				tracks [ track ].songwriter = songwriter
+			if tracks.n > 0 then
+				tracks [ tracks.n ].songwriter = songwriter
 				return
 			end
 		end
@@ -199,8 +219,8 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			if #tracks > 0 then
-				tracks [ track ].title = title
+			if tracks.n > 0 then
+				tracks [ tracks.n ].title = title
 				return
 			end
 		end
@@ -210,7 +230,8 @@ local d = {
 		local files = data.files
 		if #files > 0 then
 			local tracks = files [ #files ].tracks
-			tracks [ #tracks + 1 ] = { type = datatype , track = track , postgap = 0 , pregap = 0 , flags = { } , indexes = { } ,
+			tracks.n = track
+			tracks [ track ] = { type = datatype , track = track , flags = { } , indexes = { } ,
 				songwriter = data.songwriter , title = data.title , performer = data.performer
 			}
 			return
@@ -219,11 +240,11 @@ local d = {
 	end ;
 }
 local function doline ( data , op , ... )
-	print(op,...)
 	d [ op ] ( data , ... )
 end
 
 function read ( path )
+	-- tracks can be a holey array. the maximum index stored at tracks.n
 	local data = {
 		files = { }
 	}
