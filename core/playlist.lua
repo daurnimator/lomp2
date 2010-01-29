dcd47479e205b3f49365156766133c65c76a10aa
daurnimator <daurnimator@8385cabc-f040-0410-9bc4-65f6fe3c7374>
2010-01-29 17:37:58 +0000

    * playlist.delete trigger: no longer tries to fetch deleted playlist (it doesn't exist!)
    git-svn-id: http://lomp.googlecode.com/svn/trunk@246 8385cabc-f040-0410-9bc4-65f6fe3c7374

--- core/playlist.lua
+++ core/playlist.lua
@@ -195,7 +195,7 @@ function sort ( id , eq )
 end
 
 core.triggers.register ( "playlist_create", function ( plnum ) local pl = core.playlist.getplaylist ( plnum ); updatelog ( "Created playlist #" .. plnum .. ": '" .. pl.name .. "'" , 4 ) end , false , true )
-core.triggers.register ( "playlist_delete", function ( plnum ) local pl = core.playlist.getplaylist ( plnum ); updatelog ( "Deleted playlist #" .. plnum , 4 ) end , false , true )
+core.triggers.register ( "playlist_delete", function ( plnum ) updatelog ( "Deleted playlist #" .. plnum , 4 ) end , false , true )
 core.triggers.register ( "playlist_clear", function ( plnum ) local pl = core.playlist.getplaylist ( plnum ); updatelog ( "Cleared playlist #" .. plnum .. " (" .. pl.name .. ")" , 4 ) end , false , true )
 core.triggers.register ( "playlist_sort", function ( plnum ) local pl = core.playlist.getplaylist ( plnum ); updatelog ( "Sorted playlist #" .. plnum .. " (" .. pl.name .. ")" , 4 ) end , false , true )
 core.triggers.register ( "playlist_newrevision", function ( plnum , revision ) local pl = core.playlist.getplaylist ( plnum ); updatelog ( "Playlist #" .. plnum .. " (" .. pl.name .. ") has a new revision" , 4 ) end , false , true )
